0ae06588d3343150c73c991dbf0a77ab62929cf9
Vandal-William <vandal.william@hotmail.com>
2022-10-30 14:30:25 +0100

    Fix: bottom navigation

--- src/components/BottomBar/index.js
+++ src/components/BottomBar/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/system';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
@@ -8,8 +9,15 @@ import { Link } from 'react-router-dom';
 function BottomBar() {
   const [value, setValue] = React.useState(0);
 
+  const NavBottom = styled(Box)(({ theme }) => ({
+    [theme.breakpoints.between('xs', 'sm')]: {
+        display: 'block'
+    },
+  }));
+
+
   return (
-    <Box sx={{ width: '100%' }}>
+    <NavBottom sx={{ width: '100%', display: 'none' }}>
       <BottomNavigation
         showLabels
         value={value}
@@ -20,7 +28,7 @@ function BottomBar() {
       > 
         <BottomNavigationAction component={Link} to='/'  icon={<HomeIcon sx={{color: 'white'}}/>} />
       </BottomNavigation>
-    </Box>
+    </NavBottom>
   );
 }
 
